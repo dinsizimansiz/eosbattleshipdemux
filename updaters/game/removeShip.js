9a8381af27e3746b7f246d11034592f67db9ba7f
dinsizimansiz <oguzvuruskaner/@gmail.com>
2018-10-21 01:14:11 +0300

    Added removeShip action.

--- updaters/game/removeShip.js
+++ updaters/game/removeShip.js
@@ -1,1 +1,46 @@
-//TODO
+const {getUser,shipGetter,updateGame} = require("utils");
+
+async function removeShip(state, payload, blockInfo, context)
+{
+    var userid = payload.data.player;
+    var shipName = payload.data.shipname;
+    var games = state.games;
+
+    try
+    {
+        var game;
+        var user;
+        await games.findOne({$or: [{host: {userid: userid}}, {challenger: {userid: userid}}]})
+            .toArray()
+            .then((data) => {
+                game = data[0];
+                user = getUser(game, userid);
+                user.playerTable = _removeShip(user.playerTable, shipName);
+            });
+
+        game = updateGame(game,userid,user);
+        await games.updateOne({$or: [{host: {userid: userid}}, {challenger: {userid: userid}}]},game);
+    }
+
+
+    catch(err)
+    {
+        console.error(err);
+    }
+}
+
+
+_removeShip = function(playerTable, shipName) {
+
+    var {shipChar} = shipGetter(shipName);
+    playerTable.forEach(function(item,index,arr) {
+        if(item === shipChar)
+        {
+            arr[index] = "0";
+        }
+    });
+    return playerTable;
+};
+
+
+module.exports = removeShip;
